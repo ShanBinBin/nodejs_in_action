67b88b4512721ca45105fd80c02ffc6823674412
sx-9527 <sx-shanbb@dtdream.com>
2018-12-17 10:19:48 +0800

    Add Connect_Demo

---
+++ Connect_Demo/connect_test.js
@@ -0,0 +1,70 @@
+var connect = require('connect');
+var app = connect();
+
+function logger(req, res, next) {
+    console.log('%s %s', req.method, req.url); // 这个log打印出来的req.url是/admin,而后面的admin中间件却是/，可能是因为admin挂载了/admin的原因
+    next();
+}
+
+function hello(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello world');
+}
+
+function restrict(req, res, next) {
+    var authorization = req.headers.authorization;
+    if (!authorization) {
+        return next(new Error('Unauthorized'));
+    }
+
+    var parts = authorization.split(' ');
+    var scheme = parts[0];
+    var auth = new Buffer(parts[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+
+    authenticateWithDatabase(user, pass, function (err) {
+        if (err) {
+            return next(err);
+        }
+
+        next();
+    })
+}
+function authenticateWithDatabase(user, pass, callbak) {
+    let authorizated = false;
+    let users = [
+        {user: 'tobi', pass: 'ferret'},
+        {user: 'loki', pass: 'ferret'},
+        {user: 'jane', pass: 'ferret'}
+    ];
+    users.forEach(function (item) {
+        if (user === item.user && pass === item.pass) {
+            authorizated = true;
+        }
+    })
+    if (authorizated) {
+        callbak();
+    } else {
+        callbak('authorizated faild')
+    }
+}
+
+function admin(req, res, next) {
+    switch (req.url) {
+        case '/':
+            res.end('try /users');
+            break;
+        case '/users':
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(['tobi', 'loki', 'jane']));
+            break;
+    }
+}
+
+app.use(logger);
+app.use('/admin', restrict); // 当.use()的第一个参数是个字符串时，只有URL前缀与之匹配时，Connect才会调用后面的中间件
+app.use('/admin', admin);
+app.use(hello);
+
+app.listen(5000);
